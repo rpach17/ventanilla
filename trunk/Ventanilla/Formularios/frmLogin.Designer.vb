1024a664f29ba3a0b6538319fe06928728bc5d7c
josuetec2003@gmail.com <josuetec2003@gmail.com@42d53953-593b-9611-1b0e-0f543066d8af>
2014-03-27 22:14:49 +0000

    Diseño formulario de inicio de sesion

--- trunk/Ventanilla/Formularios/frmLogin.Designer.vb
+++ trunk/Ventanilla/Formularios/frmLogin.Designer.vb
@@ -22,8 +22,127 @@ Partial Class frmLogin
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.chkConfVentanilla = New System.Windows.Forms.CheckBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.txtPass = New System.Windows.Forms.TextBox()
+        Me.txtUser = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.btnEntrar = New System.Windows.Forms.Button()
+        Me.Panel1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'Panel1
+        '
+        Me.Panel1.BackColor = System.Drawing.Color.FromArgb(CType(CType(224, Byte), Integer), CType(CType(224, Byte), Integer), CType(CType(224, Byte), Integer))
+        Me.Panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.Panel1.Controls.Add(Me.chkConfVentanilla)
+        Me.Panel1.Controls.Add(Me.Label4)
+        Me.Panel1.Controls.Add(Me.Label3)
+        Me.Panel1.Controls.Add(Me.txtPass)
+        Me.Panel1.Controls.Add(Me.txtUser)
+        Me.Panel1.Controls.Add(Me.Label2)
+        Me.Panel1.Controls.Add(Me.btnEntrar)
+        Me.Panel1.Location = New System.Drawing.Point(5, 5)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(470, 294)
+        Me.Panel1.TabIndex = 11
+        '
+        'chkConfVentanilla
+        '
+        Me.chkConfVentanilla.AutoSize = True
+        Me.chkConfVentanilla.Location = New System.Drawing.Point(171, 167)
+        Me.chkConfVentanilla.Name = "chkConfVentanilla"
+        Me.chkConfVentanilla.Size = New System.Drawing.Size(122, 17)
+        Me.chkConfVentanilla.TabIndex = 8
+        Me.chkConfVentanilla.Text = "Configurar ventanilla"
+        Me.chkConfVentanilla.UseVisualStyleBackColor = True
+        Me.chkConfVentanilla.Visible = False
+        '
+        'Label4
+        '
+        Me.Label4.BackColor = System.Drawing.Color.Gray
+        Me.Label4.Dock = System.Windows.Forms.DockStyle.Top
+        Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.0!)
+        Me.Label4.ForeColor = System.Drawing.Color.FromArgb(CType(CType(128, Byte), Integer), CType(CType(255, Byte), Integer), CType(CType(128, Byte), Integer))
+        Me.Label4.Location = New System.Drawing.Point(0, 0)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(468, 41)
+        Me.Label4.TabIndex = 7
+        Me.Label4.Text = "Credenciales"
+        Me.Label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!)
+        Me.Label3.Location = New System.Drawing.Point(73, 140)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(92, 20)
+        Me.Label3.TabIndex = 6
+        Me.Label3.Text = "Contraseña"
+        '
+        'txtPass
+        '
+        Me.txtPass.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.0!)
+        Me.txtPass.Location = New System.Drawing.Point(171, 134)
+        Me.txtPass.Name = "txtPass"
+        Me.txtPass.PasswordChar = Global.Microsoft.VisualBasic.ChrW(42)
+        Me.txtPass.Size = New System.Drawing.Size(211, 29)
+        Me.txtPass.TabIndex = 2
+        '
+        'txtUser
+        '
+        Me.txtUser.CharacterCasing = System.Windows.Forms.CharacterCasing.Upper
+        Me.txtUser.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.0!)
+        Me.txtUser.Location = New System.Drawing.Point(171, 102)
+        Me.txtUser.Name = "txtUser"
+        Me.txtUser.Size = New System.Drawing.Size(211, 29)
+        Me.txtUser.TabIndex = 1
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!)
+        Me.Label2.Location = New System.Drawing.Point(101, 108)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(64, 20)
+        Me.Label2.TabIndex = 4
+        Me.Label2.Text = "Usuario"
+        '
+        'btnEntrar
+        '
+        Me.btnEntrar.Font = New System.Drawing.Font("Microsoft Sans Serif", 13.0!)
+        Me.btnEntrar.Location = New System.Drawing.Point(171, 187)
+        Me.btnEntrar.Name = "btnEntrar"
+        Me.btnEntrar.Size = New System.Drawing.Size(211, 47)
+        Me.btnEntrar.TabIndex = 3
+        Me.btnEntrar.Text = "Iniciar la sesión"
+        Me.btnEntrar.UseVisualStyleBackColor = True
+        '
+        'frmLogin
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.Green
+        Me.ClientSize = New System.Drawing.Size(479, 304)
+        Me.Controls.Add(Me.Panel1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
+        Me.Name = "frmLogin"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
         Me.Text = "frmLogin"
+        Me.Panel1.ResumeLayout(False)
+        Me.Panel1.PerformLayout()
+        Me.ResumeLayout(False)
+
     End Sub
+    Friend WithEvents Panel1 As System.Windows.Forms.Panel
+    Friend WithEvents chkConfVentanilla As System.Windows.Forms.CheckBox
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents txtPass As System.Windows.Forms.TextBox
+    Friend WithEvents txtUser As System.Windows.Forms.TextBox
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents btnEntrar As System.Windows.Forms.Button
 End Class
