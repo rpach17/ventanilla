50ecd91f5d4bbe83a103c4fae22690f194eee143
rpach17@gmail.com <rpach17@gmail.com@42d53953-593b-9611-1b0e-0f543066d8af>
2014-08-19 02:11:12 +0000

    nuevas funciones nuevo Formularios/frmEntregaDocs.vb

--- trunk/Ventanilla/Formularios/frmEntregaDocs.vb
+++ trunk/Ventanilla/Formularios/frmEntregaDocs.vb
@@ -4,4 +4,8 @@
         eAPPCA.TramitesEntregar(dgvTramites)
 
     End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles btnFiltrar.Click
+        eAPPCA.TramitesEntregar(dgvTramites, txtCodigoTramite.Text)
+    End Sub
 End Class
