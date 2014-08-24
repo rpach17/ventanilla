2e82507719a0e314ad6adc932f22a8797f1872e7
rpach17@gmail.com <rpach17@gmail.com@42d53953-593b-9611-1b0e-0f543066d8af>
2014-08-24 21:39:03 +0000

    ultimas actualizaciones

--- trunk/Ventanilla/Formularios/frmEntregaDocs.vb
+++ trunk/Ventanilla/Formularios/frmEntregaDocs.vb
@@ -8,4 +8,14 @@
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles btnFiltrar.Click
         eAPPCA.TramitesEntregar(dgvTramites, txtCodigoTramite.Text)
     End Sub
+
+    Private Tramitess As New List(Of TRAMITES)
+    Private Sub dgvTramites_CellClick(sender As Object, e As DataGridViewCellEventArgs) Handles dgvTramites.CellClick
+        If e.RowIndex < 0 OrElse Not e.ColumnIndex = dgvTramites.Columns("Entregar").Index Then Return
+
+        eAPPCA.entregaTramite(DameID(dgvTramites))
+        MsgBox(String.Format("Tramite {0} Entregado", DameID(dgvTramites, 1)))
+        eAPPCA.TramitesEntregar(dgvTramites)
+
+    End Sub
 End Class
